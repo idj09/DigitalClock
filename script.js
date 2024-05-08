1d7a7f1844fb7551c7da56e31cf6fb48481c72c9
idj09 <162777268+idj09@users.noreply.github.com>
2024-05-08 08:56:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const time = document.getElementById("time");
+const timeformat = document.getElementById("timeformat");
+
+document.addEventListener("DOMContentLoaded", () => {
+  setInterval(showTime, 1000);
+});
+
+const showTime = () => {
+  let date = new Date();
+  let hr = date.getHours();
+  let mins = date.getMinutes();
+  let sec = date.getSeconds();
+
+  hr = hr < 10 ? `0${hr}` : hr;
+  mins = mins < 10 ? `0${mins}` : mins;
+  sec = sec < 10 ? `0${sec}` : sec;
+
+  time.innerHTML = `${hr}: ${mins}: ${sec}`;
+
+  timeformat.innerHTML = hr > 12 ? "PM" : "AM";
+};
